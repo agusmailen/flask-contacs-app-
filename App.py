2f24d244998eb8753ed8d984e8da18562e9633f4
Agus Mailen <marrapodiagustina@gmail.com>
2021-12-16 17:47:53 -0300

    formulario, conexion a mysql

---
+++ App.py
@@ -0,0 +1,38 @@
+from re import fullmatch
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_DB'] = 'flaskcontact'
+mysql = MySQL(app)
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO contacs (fullname, phone, email) VALUES(%s, %s, %s)', (fullname, phone, email))
+        mysql.connection.commit()    
+        return 'received'
+    
+
+@app.route('/edit')
+def edit_contact():
+    return 'edit contact'
+
+@app.route('/delete')
+def delete_contact():
+    return 'delete contact'
+
+if __name__ == '__main__':
+	app.run(port = 3000, debug = True)
+    
